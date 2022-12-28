60c20c70af212b0e81bf941b4e0d1ee1a05d6247
YounSoyoung <63245740+YounSoyoung@users.noreply.github.com>
2022-12-28 16:44:42 +0900

    Update FindAllDTO.java

--- src/main/java/com/example/demo/review/dto/FindAllDTO.java
+++ src/main/java/com/example/demo/review/dto/FindAllDTO.java
@@ -1,30 +1,34 @@
-package com.example.demo.review.dto;
+package com.example.date_scheduling.post.dto;
 
-import com.example.demo.review.entity.Review;
+import com.example.date_scheduling.post.entity.Post;
 import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Setter @Getter @ToString
+@Setter
+@Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class FindAllDTO {
-    private int count; //리뷰 목록 수
+// 전체 게시물 목록 보여줄 때 필요한 객체
+public class FindAllPostDto {
 
-    private List<ReviewDTO> reviews; //userId가 빠진 리뷰 리스트
+    private int count;  // 전체 게시물 갯수
+    private List<PostDto> posts;
 
-    public FindAllDTO(List<Review> reviewList){
-        this.count = reviewList.size();
-        this.convertDtoList(reviewList);
+    public FindAllPostDto(List<Post> postList) {
+        this.count = postList.size();
+        this.convertDtoList(postList);
     }
 
-    public void convertDtoList(List<Review> reviewList){
-        List<ReviewDTO> dtos = new ArrayList<>();
+    // List<Post>를 받으면 List<PostDto>로 변환하는 메서드
+    public void convertDtoList(List<Post> postList) {
+        List<PostDto> dtos = new ArrayList<>();
 
-        for(Review review : reviewList){
-            dtos.add(new ReviewDTO(review));
+        for (Post post : postList) {
+            dtos.add(new PostDto((post)));
         }
-        this.reviews = dtos;
+        this.posts = dtos;
     }
 }
